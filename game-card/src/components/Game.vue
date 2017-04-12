26abafbdd54a9dd0fd42b9e86aa8a50ba4545e05
habilhusni <habilhusni@gmail.com>
2017-04-12 20:07:53 +0700

    adding-some-table-and-routing

---
+++ game-card/src/components/Game.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="game">
+
+    <h1> GAME PAGE </h1>
+    <div class="question-box">
+
+      <div class="question" style="width: 100%">
+        <p>QUESTION1</p>
+          <label class="custom-control custom-radio">
+            <input id="radio1" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer1</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer2</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer3</span>
+          </label>
+        <p>QUESTION2</p>
+          <label class="custom-control custom-radio">
+            <input id="radio1" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer1</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer2</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer3</span>
+          </label>
+        <p>QUESTION3</p>
+          <label class="custom-control custom-radio">
+            <input id="radio1" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer1</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer2</span>
+          </label>
+          <label class="custom-control custom-radio">
+            <input id="radio2" name="radio" type="radio" class="custom-control-input">
+            <span class="custom-control-indicator"></span>
+            <span class="custom-control-description">Answer3</span>
+          </label>
+
+          <table class="table" style="width: 100%">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>User</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>10</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>12</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>8</td>
+              </tr>
+            </tbody>
+          </table>
+
+      </div>
+
+    </div>
+
+    <router-link to="home" class="backHome">
+      Back to home
+    </router-link>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .backHome {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .question {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start; 
+  }
+
+</style>
